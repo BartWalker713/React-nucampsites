add4a2f183dff2c9b473c123fc3d0bc0fa4da98f
Bart Walker <srvwish22@gmail.com>
2020-12-31 18:01:06 -0600

    Header and Footer

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -1,16 +1,7 @@
 import { render } from "@testing-library/react";
-import { Component } from "react";
 import { Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle } from "reactstrap";
 
-class CampsiteInfo extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-           selectedCampsite: null
-        };
-    }
-
-renderCampsite(campsite) {
+function RenderCampsite({campsite}) {
     return (
         <div className="col-md-5 m-1">
             <Card>
@@ -24,7 +15,7 @@ renderCampsite(campsite) {
     )
 }
 
-    renderComments(comments) {
+    function RenderComments({comments}) {
         if (comments) {
             return (
                 <div className="col-md-5 m-1">
@@ -38,22 +29,22 @@ renderCampsite(campsite) {
         )
     }
 
-    render() {
-        if (this.props.campsite) {
+    function CampsiteInfo(props) {
+        if (props.campsite) {
             return (
                 <div className="container">
-                <div className="row">
-                    {this.renderCampsite(this.props.campsite)}
-                    {this.renderComments(this.props.campsite.comments)}
-                </div>
+                    <div className="row">
+                        <RenderCampsite campsite={props.campsite} />
+                        <RenderComments comments={props.campsite.comments} />
+                    </div>
                 </div>
             )
         }
             return (
                 <div></div>
             )
-        }    
-    }
+    }    
+    
 
 
 export default CampsiteInfo
